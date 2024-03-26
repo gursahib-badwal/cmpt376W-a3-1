938863374fc1f11daf233f1f5d89cd4721b700fe
gursahib-badwal <gursahibs156@gmail.com>
2024-03-26 16:41:34 -0700

    first-commit

---
+++ for.py
@@ -0,0 +1,15 @@
+import turtle
+
+length = 100
+
+for x in range(15):
+    turtle.forward(length)
+    turtle.left(90)
+    length = length + 20
+
+turtle.done()
+
+
+
+
+
---
+++ while.py
@@ -0,0 +1,16 @@
+import turtle
+
+x = 0 
+length = 100
+
+while x < 15:
+    turtle.forward(length)
+    turtle.left(90)
+    length = length + 20
+    x = x + 1
+
+turtle.done()
+    
+    
+
+    
